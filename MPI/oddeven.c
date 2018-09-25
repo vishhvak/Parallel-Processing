96460d57cba693a358071fd4fd28d0fa35a90b2a
Vishhvak Srinivasan <cswithvish@gmail.com>
2018-09-25 15:01:38 +0530

    Even/Odd Numbers using 2 Processes, MPI

---
+++ MPI/oddeven.c
@@ -0,0 +1,34 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+  MPI_Init (&argc, &argv);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int number;
+  if (rank == 0) {
+    
+    printf("Process %d inputting number and sending it\n", rank);
+    printf("Enter the number to be sent\n");
+    scanf("%d",&number);
+    
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  } 
+  else if (rank == 1) {
+    
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("Process %d received number %d from process 0\n", rank, number);
+    
+    if(number%2==0)
+      printf("The number recieved is even\n");
+    else
+      printf("The number recieved is odd\n");
+  }
+
+  MPI_Finalize();
+}
